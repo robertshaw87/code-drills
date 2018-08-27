edc1f6ddfd3031e052b6c05600d023700c4a60af
Robert Shaw <robertshaw87@gmail.com>
2018-08-27 13:03:40 -0700

    create initial constructor prototype activity steps

--- 11 - Constructor Functions and Algorithms/02 - Constructors and Prototype/challenge-prompt.js
+++ 11 - Constructor Functions and Algorithms/02 - Constructors and Prototype/challenge-prompt.js
@@ -1,1 +1,20 @@
-// 
+// Create a `Human` constructor function that takes in a name 
+// and creates an object with that name and an age of 0
+function Human(name){
+  this.name = name;
+  this.age = 0;
+}
+
+// Create a new Human object with a name of "Blake" and store it inside
+// the variable `blake`
+var blake = new Human("Blake");
+
+// Set the age of `blake` to be 1 and add a new method `talk` by using the `prototype` property
+// `talk` should be a function that takes in no arguments and console logs "Ma. Da."
+
+
+// Call on the `talk` method of `blake`
+
+
+// Set the age of `blake` to be 2 and add a new method `walk` by using the `prototype` property
+// `walk` should be a function that takes in no arguments and console logs "Blake walked!"
