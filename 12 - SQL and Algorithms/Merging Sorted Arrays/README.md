9325e35498ebb8d9a88bd973a0c4d9886e37c9a4
Robert Shaw <robertshaw87@gmail.com>
2018-08-28 17:04:00 -0700

    finish substring activity, mostly finish merging sorted arrays

---
+++ 12 - SQL and Algorithms/Merging Sorted Arrays/README.md
@@ -0,0 +1,7 @@
+# Merging Sorted Array
+
+### In this activity, we're going to be writing a function that takes in two sorted arrays as arguments and returns a new sorted array that contains all the elements from both arrays
+
+* Do not concatenate the two arrays together and sort the combined array afterwards
+
+* Iterate through the two arrays concurrently and construct your output array during that iteration.
---
+++ 12 - SQL and Algorithms/Merging Sorted Arrays/solution/challenge-solution.js
@@ -0,0 +1,39 @@
+// Merging Sorted Arrays
+
+
+
+// The `mergeSorted` function takes in two sorted arrays and returns a new 
+// sorted array containing all the elements of the two argument arrays
+function mergeSorted(arr1, arr2) {
+
+  // Declare variables for the indices we're tracking for the two arrays
+  var index1 = 0;
+  var index2 = 0;
+  var limit1 = arr1.length - 1;
+  var limit2 = arr2.length - 1;
+  // Initiate 
+  var mergedArr = [];
+
+  while (index1 <= limit1 && index2 <= limit2){
+
+    if (arr1[index1] < arr2[index2]) {
+      mergedArr.push(arr1[index1]);
+      index1++;
+    } else {
+      mergedArr.push(arr2[index2]);
+      index2++;
+    }
+
+  }
+
+  if (index1 > limit1 && index2 <= limit2) {
+    mergedArr = mergedArr.concat(arr2.slice(index2));
+  }
+  if (index2 > limit2 && index1 <= limit1) {
+    mergedArr = mergedArr.concat(arr1.slice(index1));
+  }
+
+  return mergedArr;
+}
+
+module.exports = mergeSorted;
