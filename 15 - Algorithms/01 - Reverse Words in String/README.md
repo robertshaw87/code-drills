0e9cc7b39b1f518bdac4080fdbda465fcf59e27f
Robert Shaw <robertshaw87@gmail.com>
2018-09-11 12:21:14 -0700

    initial explanation of warmup algo

---
+++ 15 - Algorithms/01 - Reverse Words in String/README.md
@@ -0,0 +1,3 @@
+# Reverse Words
+
+### In this activity, we're going to be writing a function that takes in a sentence as a string, reverses the individual words within the sentence, and returns the new sentence.
---
+++ 15 - Algorithms/01 - Reverse Words in String/solution/challenge-solution.js
@@ -0,0 +1,7 @@
+// Reverse Words
+
+
+// Write a function `reverseWords` that takes in a sentence as a string and
+// reverses the words within the sentence. Each word within the sentence is
+// seperated by a space and you can assume there will not be more than one
+// space between words.
