38d6795c47ec1f627189a893496a3c0c12a30f03
Robert Shaw <robertshaw87@gmail.com>
2018-10-02 10:10:13 -0700

    add hint and prompt to bracket checker

--- 23 - Algorithms/02 - Bracket Checker/hint/challenge-hint.js
+++ 23 - Algorithms/02 - Bracket Checker/hint/challenge-hint.js
@@ -6,41 +6,41 @@ const checkBracket = (str) => {
   // -------------------- Your Code Here --------------------
   // Create an object of the opening parenthesis associated with their corresponding closing parenthesis
   // We will be using this to check whether a character is a opening bracket and find the corresponding closing bracket
-  const opening = {
-    "{": "}",
-    "(": ")",
-    "[": "]"
-  }
+
+  
+
   // Create an object of the closing parenthesis associated with their corresponding opening parenthesis
   // We will be using this to check whether a character is a closing bracket 
-  const closing = {
-    "}": "{",
-    ")": "(",
-    "]": "["
-  }
+
+  
+
   // Create an array to use as a stack to keep track of the brackets we've encountered
-  const bracketStack = [];
+
+  
   // Iterate through the string
-  for (let i = 0; i < str.length; i++) {
-    let char = str[i];
+
+  
     // if the current character is an opening bracket
-    if (opening[char]) {
+
+    
       // push its corresponding closing bracket into the bracket stack
-      bracketStack.push(opening[char]);
+
+      
     // Otherwise, if the current character is a closing bracket
-    } else if (closing[char]) {
+
+    
       // If there's no opening brackets in the bracket stack or if the current bracket does not match the top bracket of the bracket stack
       // return false
-      if (bracketStack.length === 0 ||
-          bracketStack[bracketStack.length-1] !== char)
-        return false;
+
+      
       // Otherwise, if the current bracket is equal to the top bracket of the stack, pop the top bracket out
-      else bracketStack.pop();
-    }
-  }
+
+      
+
   // If the stack is empty, return true
   // otherwise, return false
-  return bracketStack.length === 0;  
+
+  
   // --------------------- End Code Area --------------------
 }
 
